84c9f789daef366815564fd19c7a74593b375f74
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2021-02-14 23:23:00 +0900

    Initial commit

---
+++ app/src/main/java/com/moon/boonsekwon/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.moon.boonsekwon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    var map: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        map = googleMap
+
+        val SEOUL = LatLng(37.56, 126.97)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국의 수도")
+        map?.addMarker(markerOptions)
+
+
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f))
+    }
+}
